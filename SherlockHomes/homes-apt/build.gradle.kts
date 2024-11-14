2244abbfac9757d0137ffd9b63fc66dae195c10d
sonjungchan <rnrmfjc@gmail.com>
2024-11-15 03:31:17 +0900

    feat: 스웨거 모둘 추가

--- SherlockHomes/homes-apt/build.gradle.kts
+++ SherlockHomes/homes-apt/build.gradle.kts
@@ -8,6 +8,7 @@ tasks.getByName("jar") {
 
 dependencies {
     implementation(project(":logging"))
+    implementation(project(":swagger"))
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
 }
