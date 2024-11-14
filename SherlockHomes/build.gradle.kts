4c13ba83814c49d13c818fe1044d3e6d3cb0b19c
sonjungchan <rnrmfjc@gmail.com>
2024-11-15 03:31:05 +0900

    feat: 스웨거 모둘 추가

--- SherlockHomes/build.gradle.kts
+++ SherlockHomes/build.gradle.kts
@@ -36,6 +36,7 @@ subprojects {
         implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
         implementation("org.jetbrains.kotlin:kotlin-reflect")
         implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+        implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
         testImplementation("org.springframework.boot:spring-boot-starter-test")
         testImplementation("io.projectreactor:reactor-test")
         testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
