6d743a779c6a98f8ab5cc186b906cbe171c8935f
sonjungchan <rnrmfjc@gmail.com>
2024-11-15 03:29:36 +0900

    feat: 아파트 매매 검색 VO, 서비스 계층 추가

---
+++ SherlockHomes/homes-apt/src/main/kotlin/org/sherlockhomes/homes/apartment/application/service/trade/ApartmentTradeSearchService.kt
@@ -0,0 +1,20 @@
+package org.sherlockhomes.homes.apartment.application.service.trade
+
+import org.sherlockhomes.homes.apartment.application.port.inbound.ApartmentSearchUseCase
+import org.sherlockhomes.homes.apartment.application.port.outbound.ApartmentSearchPort
+import org.sherlockhomes.homes.apartment.application.service.rent.vo.ApartmentTradeVO
+import org.sherlockhomes.homes.apartment.domain.ApartmentTrade
+import org.sherlockhomes.homes.apartment.domain.mapper.toVO
+import org.springframework.stereotype.Component
+
+@Component
+class ApartmentTradeSearchService(
+    private val apartmentTradeSearchPort: ApartmentSearchPort<ApartmentTrade>
+) : ApartmentSearchUseCase<ApartmentTradeVO.ApartmentTrade> {
+
+    override fun getList(lawdCd: Int, dealYmd: Int): List<ApartmentTradeVO.ApartmentTrade> =
+        apartmentTradeSearchPort.search(
+            lawdCd = lawdCd,
+            dealYmd = dealYmd,
+        ). map { it.toVO() }
+}
---
+++ SherlockHomes/homes-apt/src/main/kotlin/org/sherlockhomes/homes/apartment/application/service/trade/vo/ApartmentRentVO.kt
@@ -0,0 +1,11 @@
+package org.sherlockhomes.homes.apartment.application.service.trade.vo
+
+class ApartmentRentVO {
+    data class ApartmentTrade(
+        val id: Long,
+    )
+
+    data class Save(
+        val id: Long,
+    )
+}
