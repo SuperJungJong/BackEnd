0aa3463c74f71704b85be5b629646461022bee3e
sonjungchan <rnrmfjc@gmail.com>
2024-11-15 03:28:59 +0900

    feat: 아파트 매매 도메인 추가

--- SherlockHomes/homes-apt/src/main/kotlin/org/sherlockhomes/homes/apartment/domain/mapper/ApartmentTradeMapper.kt
+++ SherlockHomes/homes-apt/src/main/kotlin/org/sherlockhomes/homes/apartment/domain/mapper/ApartmentTradeMapper.kt
@@ -1,2 +1,40 @@
 package org.sherlockhomes.homes.apartment.domain.mapper
 
+import org.sherlockhomes.homes.apartment.application.service.rent.vo.ApartmentTradeVO
+import org.sherlockhomes.homes.apartment.domain.ApartmentTrade
+
+fun ApartmentTrade.toVO(): ApartmentTradeVO.ApartmentTrade =
+    ApartmentTradeVO.ApartmentTrade(
+        aptDong = aptDong,
+        aptNm = aptNm,
+        aptSeq = aptSeq,
+        bonbun = bonbun,
+        bubun = bubun,
+        buildYear = buildYear,
+        buyerGbn = buyerGbn,
+        cdealDay = cdealDay,
+        cdealType = cdealType,
+        dealAmount = dealAmount,
+        dealDay = dealDay,
+        dealMonth = dealMonth,
+        dealYear = dealYear,
+        dealingGbn = dealingGbn,
+        estateAgentSggNm = estateAgentSggNm,
+        excluUseAr = excluUseAr,
+        floor = floor,
+        jibun = jibun,
+        landCd = landCd,
+        landLeaseholdGbn = landLeaseholdGbn,
+        rgstDate = rgstDate,
+        roadNm = roadNm,
+        roadNmBonbun = roadNmBonbun,
+        roadNmBubun = roadNmBubun,
+        roadNmCd = roadNmCd,
+        roadNmSeq = roadNmSeq,
+        roadNmSggCd = roadNmSggCd,
+        roadNmbCd = roadNmbCd,
+        sggCd = sggCd,
+        slerGbn = slerGbn,
+        umdCd = umdCd,
+        umdNm = umdNm,
+    )
