4b2954f00cadfe5e4adf76ac165c339627c16bcb
sonjungchan <rnrmfjc@gmail.com>
2024-11-17 01:10:52 +0900

    feat: storage 모듈 추가

---
+++ SherlockHomes/storage/db-jpa/build.gradle.kts
@@ -0,0 +1,10 @@
+allOpen {
+    annotation("javax.persistence.Entity")
+    annotation("javax.persistence.MappedSuperclass")
+    annotation("javax.persistence.Embeddable")
+}
+
+dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("mysql:mysql-connector-java:8.0.33")
+}
